f726aebca0e7013552145ee03bf3df414addefaa
wishtony <vishnutoanother@gmail.com>
2024-01-27 20:32:39 +0530

    commit

---
+++ fitclub/src/components/Footer/Footer.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import "./Footer.css";
+import Github from "../../assets/github.png";
+import Instagram from "../../assets/instagram.png";
+import LinkedIn from "../../assets/linkedin.png";
+import Logo from "../../assets/logo.png";
+
+function Footer() {
+  return (
+    <div className="footer-container">
+      <hr style={{background:"white"}}/>
+      <div className="footer">
+        <div className="social-links">
+        <img src={Github} alt="" />
+        <img src={Instagram} alt="" />
+        <img src={LinkedIn} alt="" />
+        </div>
+      
+
+      <div className="log-f">
+        <img src={Logo} alt="" />
+      </div>
+   
+      <div className="blur blur-f-1"></div>
+      <div className="blur blur-f-2"></div>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
--- fitclub/src/components/Testimonials/Testimonials.jsx
+++ fitclub/src/components/Testimonials/Testimonials.jsx
@@ -3,8 +3,9 @@ import "./Tostimonials.css";
 import { testimonialsData } from "../../data/testimonialsData";
 import leftArrow from "../../assets/leftArrow.png";
 import rightArrow from "../../assets/rightArrow.png";
-
+import { motion } from "framer-motion";
 function Testimonials() {
+  const transition = { type: "spring", duration: 3 };
   const [seleted, setSeleted] = useState(0);
   const tlength = testimonialsData.length;
 
@@ -14,7 +15,13 @@ function Testimonials() {
         <span>Testimonials</span>
         <span className="stroke-text">What they</span>
         <span>say about us</span>
-        <span>{testimonialsData[seleted].review}</span>
+        <motion.span
+         key={seleted}
+         initial={{opacity:0,x:100}}
+         animate={{opacity:1,x:0}}
+         exit={{opacity:0,x:-100}}
+         transition={transition}
+        >{testimonialsData[seleted].review}</motion.span>
         <span>
           <span style={{ color: "var(--orange)" }}>
             {testimonialsData[seleted].name}
@@ -23,9 +30,23 @@ function Testimonials() {
         </span>
       </div>
       <div className="right-t">
-        <div></div>
-        <div></div>
-        <img src={testimonialsData[seleted].image} alt="" />
+        <motion.div 
+         initial={{ opacity: 0, x: -100 }}
+         transition={{ ...transition,duration:2 }}
+         whileInView={{opacity:1,x:0}}
+        ></motion.div>
+        <motion.div
+          initial={{ opacity: 0, x: 100 }}
+          transition={{ ...transition,duration:2 }}
+          whileInView={{opacity:1,x:0}}
+        ></motion.div>
+        <motion.img 
+        key={seleted}
+        initial={{opacity:0,x:100}}
+        animate={{opacity:1,x:0}}
+        exit={{opacity:0,x:-100}}
+        transition={transition}
+        src={testimonialsData[seleted].image} alt="" />
         <div className="arrows">
           <img
             onClick={() => {
