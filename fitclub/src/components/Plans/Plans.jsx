5cd5b32d95fac0de4e10642785ce3d6af214b763
wishtony <vishnutoanother@gmail.com>
2024-01-29 17:45:07 +0530

    commit

--- fitclub/src/components/Plans/Plans.jsx
+++ fitclub/src/components/Plans/Plans.jsx
@@ -8,7 +8,7 @@ function Plans() {
     <div className="plans-container">
       <div className="blur plans-blur-1"></div>
       <div className="blur plans-blur-2"></div>
-      <div className="programs-header" style={{ gap: "2rem"}}>
+      <div className="programs-header" style={{ gap: "2rem" }}>
         <span className="stroke-text">READY TO START</span>
         <span>YOUR JOURNEY</span>
         <span className="stroke-text">NOW WITHUS</span>
