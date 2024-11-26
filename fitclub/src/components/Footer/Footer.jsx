48ac89d983a984424cb9e2e5646b72fa01d179a7
vishnu KP <vishnuto16@gmail.com>
2024-11-26 18:19:14 +0530

    new commit

--- fitclub/src/components/Footer/Footer.jsx
+++ fitclub/src/components/Footer/Footer.jsx
@@ -8,7 +8,21 @@ import Logo from "../../assets/logo.png";
 function Footer() {
   return (
     <div className="footer-container">
+      <hr style={{ background: "white" }} />
+      <div className="footer">
+        <div className="social-links">
+          <img src={Github} alt="" />
+          <img src={Instagram} alt="" />
+          <img src={LinkedIn} alt="" />
+        </div>
 
+        <div className="log-f">
+          <img src={Logo} alt="" />
+        </div>
+
+        <div className="blur blur-f-1"></div>
+        <div className="blur blur-f-2"></div>
+      </div>
     </div>
   );
 }
